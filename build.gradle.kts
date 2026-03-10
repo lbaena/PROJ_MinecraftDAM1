e2f00722e60108e92c0856bc5c84e5a248a430eb
Lluc Baena Llansola <llucbaenallansola@gmail.com>
2026-03-10 21:51:13 +0100

    add libraries implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://gitlab.com/api/v4/projects/66006947/packages/maven") }
+    maven { url = uri("https://gitlab.com/api/v4/projects/56672451/packages/maven") }
 }
 
 val junitVersion = "5.12.1"
@@ -36,6 +38,11 @@ javafx {
 }
 
 dependencies {
+    implementation("vicent.Bellver:missatges_i_alertes:4.0")
+    implementation("vicent.Bellver:Fitxers:25.26")
+    implementation("org.projectlombok:lombok:1.18.42")
+    implementation("com.google.zxing:core:3.5.4")
+    implementation("com.google.zxing:javase:3.5.4")
     testImplementation("org.junit.jupiter:junit-jupiter-api:${junitVersion}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
 }
